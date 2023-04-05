b4b8ebdbeaf551aa735cc7b681be2166ecf6baa7
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2023-04-05 00:48:40 -0500

    commit tree 1c34fcc888a916ce98501f4aab7ec85783d9f3e0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require "pry"
+
 class Triangle
-  # write code here
+  attr_reader :s1, :s2, :s3
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    errorMess
+    if s1 == s2 && s2 == s3
+      :equilateral
+    elsif s1 == s2 || s2 == s3 || s3 == s1
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def errorMess
+    if s1 <= 0 || s2 <= 0 || s3 <= 0
+      raise TriangleError
+    elsif s1 + s2 <= s3 || s1 + s3 <= s2 || s2 + s3 <= s1
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
+
+
+binding.pry
+
